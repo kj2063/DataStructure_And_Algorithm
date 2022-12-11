d9763f5d4d244175228a6057babbb8bd8f068a7c
Jun Kwon <kwonjun8890@gmail.com>
2022-12-11 23:35:29 +0900

    Implement RedBlackTree Insert function
    Implement RedBlackTree Insert function

---
+++ RedBlackTree/MyRedBlackTree.hpp
@@ -0,0 +1,211 @@
+#include <iostream>
+
+using namespace std;
+
+enum Color
+{
+    RED,
+    BLACK
+};
+
+template <typename T>
+struct Node {
+    int key;
+    T data;
+
+    Node<T> *parent = nullptr;
+    Node<T> *right = nullptr;
+    Node<T> *left = nullptr;
+
+    Color color = BLACK;
+    
+    Node(){};
+    Node(int keyParam, T dataParam):key{keyParam}, data{dataParam}{};
+};
+
+template <typename T>
+class MyRedBlackTree {
+    private:
+        Node<T> *root;
+        Node<T> *nil;
+
+    public:
+        string enumToString(Color color){
+            switch(color){
+                case 0 : return "RED";
+                case 1 : return "BLACK";
+            }
+        }
+
+        MyRedBlackTree(){
+            nil = new Node<T>;
+            root = nil;
+        };
+        ~MyRedBlackTree(){};
+
+        void insertNode(Node<T> *nodeParam);
+        void rotateRight(Node<T> *centerNode);
+        void rotateLeft(Node<T> *centerNode);
+        void arrangeInsertedNode(Node<T> *insertedNode);
+
+        void showAll();
+        void preOrder(Node<T> *cursorParam);
+
+        
+};
+
+template <typename T>
+void MyRedBlackTree<T>::insertNode(Node<T> *newNode){
+    newNode->left = nil;
+    newNode->right = nil;
+
+    if(root == nil){
+        newNode->color = BLACK;
+
+        this->root = newNode;
+    }
+    else{
+        newNode->color = RED;
+
+        Node<T> *ptr = root;
+        Node<T> *prePtr = nullptr; 
+
+        while(ptr != nil){
+            prePtr = ptr;
+
+            if(ptr->key > newNode->key)
+                ptr = prePtr->left;
+            else
+                ptr = prePtr->right;    
+        }
+        
+        newNode->parent = prePtr;
+        
+        if(prePtr->key > newNode->key){
+            prePtr->left = newNode;
+        }
+        else{
+            prePtr->right = newNode;
+        }
+
+        arrangeInsertedNode(newNode);
+    }
+};
+
+template <typename T>
+void MyRedBlackTree<T>::rotateRight(Node<T> *centerNode){
+    if(centerNode == nil || centerNode->left == nil)
+        throw invalid_argument("error : target Node == nil || targetNode->left == nil");
+    
+    //temp = will be centerNode
+    Node<T> *temp = centerNode->left;
+
+    centerNode->left = centerNode->left->right;
+
+    if(centerNode->left != nil)
+        centerNode->left->parent = centerNode;
+
+    temp->right = centerNode;
+    temp->parent = centerNode->parent;
+
+    if(temp->parent != nullptr){
+        if(temp->parent->left == centerNode)
+            temp->parent->left = temp;
+        else
+            temp->parent->right = temp;
+    }
+
+    centerNode->parent = temp;
+
+    if(root == centerNode){
+        root = temp;   
+    }
+};
+
+template <typename T>
+void MyRedBlackTree<T>::rotateLeft(Node<T> *centerNode){
+    if(centerNode == nil || centerNode->right == nil)
+        throw invalid_argument("error : target Node == nil || targetNode->right == nil");
+    
+    //temp = will be centerNode
+    Node<T> *temp = centerNode->right;
+
+    centerNode->right = centerNode->right->left;
+
+    if(centerNode->right != nil)
+        centerNode->right->parent = centerNode;
+
+    temp->left = centerNode;
+    temp->parent = centerNode->parent;
+
+    if(temp->parent != nullptr){
+        if(temp->parent->left == centerNode)
+            temp->parent->left = temp;
+        else
+            temp->parent->right = temp;
+    }
+
+    centerNode->parent = temp;
+
+    if(root == centerNode){
+        root = temp;   
+    }
+};
+
+template <typename T>
+void MyRedBlackTree<T>::arrangeInsertedNode(Node<T> *insertedNode){
+    Node<T> *parentNode = insertedNode->parent;
+    while(parentNode != nullptr && parentNode->color == RED){
+        Node<T> *grandParentNode = parentNode->parent;
+        bool isParentLeftChild = grandParentNode->left == parentNode ? true : false;
+        Node<T> *uncleNode = isParentLeftChild ? grandParentNode->right : grandParentNode->left;
+
+        if(uncleNode->color == RED){
+            parentNode->color = BLACK;
+            uncleNode->color = BLACK;
+            grandParentNode->color = RED;
+
+            insertedNode = grandParentNode;
+            parentNode = grandParentNode->parent;
+        }
+        else{
+            // to align grandParentNode - parentNode - insertedNode
+            if(insertedNode == (isParentLeftChild ? parentNode->right : parentNode->left)){
+                isParentLeftChild ? rotateLeft(parentNode) : rotateRight(parentNode);
+                // if rotation occurs, parentNode becomes child of insertedNode, so change parentNode to insertedNode
+                parentNode = insertedNode;
+            }
+
+            parentNode->color = BLACK;
+            grandParentNode->color = RED;
+            isParentLeftChild ? rotateRight(grandParentNode) : rotateLeft(grandParentNode);
+        }
+    }
+    root->color = BLACK;
+};
+
+template <typename T>
+void MyRedBlackTree<T>::showAll(){
+    cout << "======Show All======" << endl;
+    
+    preOrder(root);
+    
+    cout << "====================" << endl;
+};
+
+template <typename T>
+void MyRedBlackTree<T>::preOrder(Node<T> *ptr){
+    if(ptr != nil){
+        string direction = "root";
+        if(ptr->parent != nullptr)
+            direction = ptr->parent->right == ptr ? "R" : "L";
+
+        int parentKey = 0;
+        if(ptr->parent != nullptr) 
+            parentKey = ptr->parent->key;
+
+        cout << ptr->key << "[" << ptr->data << "] " << "(" << enumToString(ptr->color) << "," << parentKey << "," << direction << ")" << endl;
+        preOrder(ptr->left);
+        preOrder(ptr->right);
+    }
+};
---
+++ RedBlackTree/main.cpp
@@ -0,0 +1,20 @@
+#include "MyRedBlackTree.hpp"
+
+int main(){
+
+    MyRedBlackTree<char> RedBlackTree;
+    RedBlackTree.insertNode(new Node<char>(5,'A'));
+    RedBlackTree.insertNode(new Node<char>(4,'B'));
+    RedBlackTree.insertNode(new Node<char>(3,'C'));
+    RedBlackTree.insertNode(new Node<char>(2,'D'));
+    RedBlackTree.insertNode(new Node<char>(1,'E'));
+    RedBlackTree.insertNode(new Node<char>(9,'F'));
+    RedBlackTree.insertNode(new Node<char>(6,'G'));
+    RedBlackTree.insertNode(new Node<char>(10,'H'));
+    RedBlackTree.insertNode(new Node<char>(12,'I'));
+    RedBlackTree.insertNode(new Node<char>(11,'J'));
+
+    RedBlackTree.showAll();
+
+    return 0;
+}
