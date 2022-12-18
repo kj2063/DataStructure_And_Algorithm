608100ced3e8e79a276a3178201274de18183d8a
Jun Kwon <102341633+kj2063@users.noreply.github.com>
2022-12-18 21:54:04 +0900

    add delete funtion

--- RedBlackTree/MyRedBlackTree.hpp
+++ RedBlackTree/MyRedBlackTree.hpp
@@ -48,6 +48,11 @@ class MyRedBlackTree {
         void rotateLeft(Node<T> *centerNode);
         void arrangeInsertedNode(Node<T> *insertedNode);
 
+        void deleteNode(int key);
+        Node<T>* getNode(int key);
+        Node<T>* getReplacementNode(Node<T> *willReplacedNode);
+        void arrangeAfterDeleteNode(Node<T> *replacedNode, Node<T> *replacedParentNode);
+
         void showAll();
         void preOrder(Node<T> *cursorParam);
 
@@ -183,6 +188,199 @@ void MyRedBlackTree<T>::arrangeInsertedNode(Node<T> *insertedNode){
     }
     root->color = BLACK;
 };
+
+template <typename T>
+Node<T>* MyRedBlackTree<T>::getNode(int keyParam){
+    
+    Node<T> *ptr = root;
+
+    while(ptr != nil){
+        if(ptr->key == keyParam){
+            return ptr;
+        }
+        else{
+            if(ptr->key > keyParam)
+                ptr = ptr->left;
+            else
+                ptr = ptr->right;
+        }
+    }
+    
+    throw invalid_argument("invalid key");
+}
+
+
+template <typename T>
+Node<T>* MyRedBlackTree<T>::getReplacementNode(Node<T> *targetNodeParam){
+    Node<T> *ptr = targetNodeParam->right;
+
+    while(ptr->left != nil){
+        ptr = ptr->left;
+    }
+
+    return ptr;
+}
+
+template <typename T>
+void MyRedBlackTree<T>::deleteNode(int keyParam){
+    Node<T> *targetNode = getNode(keyParam);
+
+    Node<T> *afterReplacedTargetNode;
+    Node<T> *afterReplacedParentNode;
+    bool needArrange = false;
+
+    if(targetNode->right != nil && targetNode->left != nil){
+        Node<T> *selectedChildNode = getReplacementNode(targetNode);
+
+        if(selectedChildNode->color == BLACK && selectedChildNode->right->color == RED){
+            selectedChildNode->right->color = BLACK;
+        }
+        else if(selectedChildNode->color == BLACK && selectedChildNode->right->color == BLACK){
+            needArrange = true;
+        }
+
+        if(targetNode->right != selectedChildNode){
+
+            selectedChildNode->parent->left = selectedChildNode->right;
+            if(selectedChildNode != nil){
+                selectedChildNode->right->parent = selectedChildNode->parent;
+            }
+        }
+        else{
+            targetNode->right = selectedChildNode->right;
+            selectedChildNode->right->parent = targetNode;
+        }
+
+        targetNode->key = selectedChildNode->key;
+        targetNode->data = selectedChildNode->data;
+
+        afterReplacedTargetNode = selectedChildNode->right;
+        afterReplacedParentNode = selectedChildNode->parent;
+
+        delete selectedChildNode;
+    }
+    else {
+        Node<T> *selectedChildNode = targetNode->left != nil ? targetNode->left : targetNode->right;
+
+        if(targetNode->color == BLACK && selectedChildNode->color == RED){
+            selectedChildNode->color = BLACK;
+        }
+        else if(targetNode->color == BLACK && selectedChildNode->color == BLACK){
+            needArrange = true;
+        }
+
+        if(targetNode == root){
+            root = selectedChildNode;
+        }
+        
+        if(targetNode != root){
+            if(targetNode->parent->right == targetNode)
+                targetNode->parent->right = selectedChildNode;
+            else
+                targetNode->parent->left = selectedChildNode;
+        }
+
+        if(selectedChildNode != nil){
+            selectedChildNode->parent = targetNode->parent;
+        }
+
+        afterReplacedTargetNode = selectedChildNode;
+        afterReplacedParentNode = targetNode->parent;
+
+        delete targetNode;
+    }
+
+    if(needArrange){
+        arrangeAfterDeleteNode(afterReplacedTargetNode,afterReplacedParentNode);
+    }
+}
+
+template <typename T>
+void MyRedBlackTree<T>::arrangeAfterDeleteNode(Node<T> *targetNode, Node<T> *parentNode){
+
+    string isNil = targetNode == nil ? "is nil" : "is not nil";
+    cout<< "targetNode " << isNil << endl;
+    // cout << "here" << targetNode->key << endl;
+    // cout << "here" << targetNode->parent->data << endl;
+
+    while (nullptr != parentNode)
+	{
+		bool left = parentNode->left == targetNode;
+		Node<T> *siblingNode = left ? parentNode->right : parentNode->left;
+		if (RED == parentNode->color && BLACK == siblingNode->left->color && BLACK == siblingNode->right->color)
+		{
+            // case 1
+            // parent : red, sibling : black, sibling left : black, sibling right : black
+            siblingNode->color = RED;
+            parentNode->color = BLACK;
+            break; 
+		}
+		else if(BLACK == siblingNode->left->color && BLACK == siblingNode->right->color)
+		{
+			if (BLACK == siblingNode->color)
+			{
+				// case 4
+				// parent : black, sibling : black, sibling left : black, sibling right : black
+				siblingNode->color = RED;
+				targetNode = parentNode;
+				parentNode = parentNode->parent;
+				continue; 
+			}
+			else // RED == sibling->color
+			{
+				// case 5
+				// parent : black, sibling : red, sibling left : black, sibling right : black
+				parentNode->color = RED;
+				siblingNode->color = BLACK;
+				if (left)
+				{
+					rotateLeft(parentNode);
+					siblingNode = parentNode->right;
+				}
+				else
+				{
+					rotateRight(parentNode);
+					siblingNode = parentNode->left;
+				}
+				continue;
+			}
+		}
+
+		if (BLACK == siblingNode->color &&
+			((left && RED == siblingNode->left->color && BLACK == siblingNode->right->color)
+				|| (!left && BLACK == siblingNode->left->color && RED == siblingNode->right->color)))
+		{
+			// case 3
+			// parent : all, sibling : black, sibling left : red, sibling right : black
+			if (left)
+			{
+				rotateRight(siblingNode);
+				parentNode->right->color = BLACK;
+				parentNode->right->right->color = RED;
+				siblingNode = parentNode->right;
+			}
+			else
+			{
+				rotateLeft(siblingNode);
+				parentNode->left->color = BLACK;
+				parentNode->left->left->color = RED;
+				siblingNode = parentNode->left;
+			}
+		}
+		
+		if (BLACK == siblingNode->color && ((left && RED == siblingNode->right->color) || (!left && RED == siblingNode->left->color)))
+		{
+			// case 2
+			// parent : all, sibling : black, sibling left : all, sibling right : red
+			left ? rotateLeft(parentNode) : rotateRight(parentNode);
+			Color temp = parentNode->color;
+			parentNode->color = siblingNode->color;
+			siblingNode->color = temp;
+			left ? siblingNode->right->color = BLACK : siblingNode->left->color = BLACK;
+			break; 
+		}
+	}
+}
 
 template <typename T>
 void MyRedBlackTree<T>::showAll(){
--- RedBlackTree/main.cpp
+++ RedBlackTree/main.cpp
@@ -8,11 +8,14 @@ int main(){
     RedBlackTree.insertNode(new Node<char>(3,'C'));
     RedBlackTree.insertNode(new Node<char>(2,'D'));
     RedBlackTree.insertNode(new Node<char>(1,'E'));
-    RedBlackTree.insertNode(new Node<char>(9,'F'));
+    RedBlackTree.insertNode(new Node<char>(7,'F'));
     RedBlackTree.insertNode(new Node<char>(6,'G'));
     RedBlackTree.insertNode(new Node<char>(10,'H'));
     RedBlackTree.insertNode(new Node<char>(12,'I'));
     RedBlackTree.insertNode(new Node<char>(11,'J'));
+    RedBlackTree.insertNode(new Node<char>(8,'K'));
+
+    RedBlackTree.deleteNode(5);
 
     RedBlackTree.showAll();
 
